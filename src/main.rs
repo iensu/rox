292851902750fa41fe2b94e1b60ccd5efcfeca6c
Jens Östlund <jostlund@gmail.com>
2022-10-30 20:20:43 +0100

    Add interactive prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs, io};
+
+use eyre::Result;
+
+fn main() -> Result<()> {
+    let mut args = env::args();
+
+    if args.len() > 2 {
+        eprintln!("Usage: rox [script]");
+        panic!();
+    } else if args.len() == 2 {
+        run_file(args.next().unwrap());
+    } else {
+        run_prompt()?;
+    }
+
+    Ok(())
+}
+
+fn run_file(file_path: String) {
+    let program = fs::read_to_string(file_path).expect("Failed to read file");
+    run(program);
+}
+
+fn run(program: String) {
+    println!("input: {program}")
+}
+
+fn run_prompt() -> Result<()> {
+    let mut buffer = String::new();
+
+    println!("Rox prompt, hit ctrl-d to quit.");
+
+    loop {
+        print!("> ");
+        let bytes_read = io::stdin().read_line(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+
+        run(buffer.to_string());
+        buffer.clear();
+    }
+
+    Ok(())
 }
