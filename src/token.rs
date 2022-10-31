f8d7dc7e80ca2a53711a61c9c083a2fb0575330c
Jens Östlund <jostlund@gmail.com>
2022-10-31 12:45:27 +0100

    Add support for more multichar tokens

--- src/token.rs
+++ src/token.rs
@@ -3,7 +3,7 @@ use std::{iter::Peekable, str::Chars};
 use eyre::{eyre, Result};
 
 #[allow(non_camel_case_types)]
-#[derive(Debug, PartialEq, Copy, Clone)]
+#[derive(Debug, PartialEq)]
 pub enum TokenType {
     // Single character tokens
     LEFT_PAREN,
@@ -51,26 +51,34 @@ pub enum TokenType {
     EOF,
 }
 
-#[derive(Debug, PartialEq, Copy, Clone)]
+#[derive(Debug, PartialEq)]
 pub enum Literal {
     Null,
 }
 
-#[derive(Debug, PartialEq, Copy, Clone)]
-pub struct Token<'a> {
+#[derive(Debug, PartialEq)]
+pub struct Token {
     pub token_type: TokenType,
-    pub lexeme: &'a str,
+    pub lexeme: String,
     pub literal: Literal, // Here be monsters
     pub line: usize,
+    pub column: usize,
 }
 
-impl<'a> Token<'a> {
-    pub fn new(token_type: TokenType, lexeme: &'a str, literal: Literal, line: usize) -> Self {
+impl Token {
+    pub fn new(
+        token_type: TokenType,
+        lexeme: String,
+        literal: Literal,
+        line: usize,
+        column: usize,
+    ) -> Self {
         Self {
             token_type,
             lexeme,
             literal,
             line,
+            column,
         }
     }
 }
@@ -79,8 +87,8 @@ pub struct Scanner<'a> {
     start: usize,
     current: usize,
     line: usize,
+    column: usize,
     source: &'a str,
-    tokens: Vec<Token<'a>>,
 }
 
 impl<'a> Scanner<'a> {
@@ -89,64 +97,96 @@ impl<'a> Scanner<'a> {
             start: 0,
             current: 0,
             line: 1,
-            tokens: vec![],
+            column: 0,
             source,
         }
     }
 
     pub fn scan_tokens(&mut self) -> Result<Vec<Token>> {
         let mut chars = self.source.chars().peekable();
+        let mut tokens: Vec<Token> = Vec::new();
 
         while self.current < self.source.len() {
             self.start = self.current;
             self.current += 1;
 
-            self.scan_token(&mut chars)?;
+            let token = self.scan_token(&mut chars)?;
+            tokens.push(token);
         }
 
-        self.tokens
-            .push(Token::new(TokenType::EOF, "", Literal::Null, self.line));
+        tokens.push(Token::new(
+            TokenType::EOF,
+            "".into(),
+            Literal::Null,
+            self.line,
+            self.current,
+        ));
 
-        Ok(self.tokens.clone())
+        Ok(tokens)
     }
 
-    fn scan_token(&mut self, chars: &mut Peekable<Chars>) -> Result<()> {
+    fn scan_token(&mut self, chars: &mut Peekable<Chars>) -> Result<Token> {
         use TokenType::*;
 
         let c = chars.next().unwrap();
 
         match c {
-            '(' => self.add_token(LEFT_PAREN),
-            ')' => self.add_token(RIGHT_PAREN),
-            '{' => self.add_token(LEFT_BRACE),
-            '}' => self.add_token(RIGHT_BRACE),
-            ',' => self.add_token(COMMA),
-            '.' => self.add_token(DOT),
-            '-' => self.add_token(MINUS),
-            '+' => self.add_token(PLUS),
-            ';' => self.add_token(SEMICOLON),
-            '*' => self.add_token(STAR),
+            '(' => Ok(self.create_token(LEFT_PAREN)),
+            ')' => Ok(self.create_token(RIGHT_PAREN)),
+            '{' => Ok(self.create_token(LEFT_BRACE)),
+            '}' => Ok(self.create_token(RIGHT_BRACE)),
+            ',' => Ok(self.create_token(COMMA)),
+            '.' => Ok(self.create_token(DOT)),
+            '-' => Ok(self.create_token(MINUS)),
+            '+' => Ok(self.create_token(PLUS)),
+            ';' => Ok(self.create_token(SEMICOLON)),
+            '*' => Ok(self.create_token(STAR)),
             '!' => {
                 if let Some(_) = chars.next_if_eq(&'=') {
                     self.current += 1;
-                    self.add_token(BANG_EQUAL)
+                    Ok(self.create_token(BANG_EQUAL))
                 } else {
-                    self.add_token(BANG)
+                    Ok(self.create_token(BANG))
+                }
+            }
+            '>' => {
+                if let Some(_) = chars.next_if_eq(&'=') {
+                    self.current += 1;
+                    Ok(self.create_token(GREATER_EQUAL))
+                } else {
+                    Ok(self.create_token(GREATER))
+                }
+            }
+            '<' => {
+                if let Some(_) = chars.next_if_eq(&'=') {
+                    self.current += 1;
+                    Ok(self.create_token(LESS_EQUAL))
+                } else {
+                    Ok(self.create_token(LESS))
+                }
+            }
+            '=' => {
+                if let Some(_) = chars.next_if_eq(&'=') {
+                    self.current += 1;
+                    Ok(self.create_token(EQUAL_EQUAL))
+                } else {
+                    Ok(self.create_token(EQUAL))
                 }
             }
             _ => {
-                return Err(eyre!("Line {}: Unexpected character '{c}'", self.line));
+                return Err(eyre!(
+                    "Line {} Col {}: Unexpected character '{c}'",
+                    self.line,
+                    self.start
+                ));
             }
         }
-
-        Ok(())
     }
 
-    fn add_token(&mut self, t: TokenType) {
+    fn create_token(&mut self, t: TokenType) -> Token {
         let lexeme = &self.source[self.start..self.current];
 
-        self.tokens
-            .push(Token::new(t, lexeme, Literal::Null, self.line));
+        Token::new(t, lexeme.into(), Literal::Null, self.line, self.start)
     }
 }
 
@@ -158,42 +198,38 @@ mod test {
 
     #[test]
     fn single_tokens_are_parsed_correctly() {
-        let source = "(){},.-+;*!";
-        let expected = vec![
-            Token::new(LEFT_PAREN, "(", L::Null, 1),
-            Token::new(RIGHT_PAREN, ")", L::Null, 1),
-            Token::new(LEFT_BRACE, "{", L::Null, 1),
-            Token::new(RIGHT_BRACE, "}", L::Null, 1),
-            Token::new(COMMA, ",", L::Null, 1),
-            Token::new(DOT, ".", L::Null, 1),
-            Token::new(MINUS, "-", L::Null, 1),
-            Token::new(PLUS, "+", L::Null, 1),
-            Token::new(SEMICOLON, ";", L::Null, 1),
-            Token::new(STAR, "*", L::Null, 1),
-            Token::new(BANG, "!", L::Null, 1),
-            Token::new(EOF, "", L::Null, 1),
+        let test_cases = vec![
+            ("(", LEFT_PAREN),
+            (")", RIGHT_PAREN),
+            ("{", LEFT_BRACE),
+            ("}", RIGHT_BRACE),
+            (",", COMMA),
+            (".", DOT),
+            ("-", MINUS),
+            ("+", PLUS),
+            (";", SEMICOLON),
+            ("*", STAR),
+            ("!", BANG),
+            ("<", LESS),
+            (">", GREATER),
+            ("=", EQUAL),
+            ("!=", BANG_EQUAL),
+            ("<=", LESS_EQUAL),
+            (">=", GREATER_EQUAL),
+            ("==", EQUAL_EQUAL),
         ];
 
-        let mut scanner = Scanner::new(source);
-        let tokens = scanner.scan_tokens().expect("Program can be scanned");
+        for (source, token_type) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner
+                .scan_tokens()
+                .expect(&format!("Failed to scan token '{source}'"));
+            let expected = vec![
+                Token::new(token_type, source.to_string(), L::Null, 1, 0),
+                Token::new(EOF, "".into(), L::Null, 1, source.len()),
+            ];
 
-        assert_eq!(tokens, expected);
-    }
-
-    #[test]
-    fn two_char_tokens_are_parsed_correctly() {
-        let source = "!=<=>===";
-        let expected = vec![
-            Token::new(BANG_EQUAL, "!=", L::Null, 1),
-            Token::new(LESS_EQUAL, "<=", L::Null, 1),
-            Token::new(GREATER_EQUAL, ">=", L::Null, 1),
-            Token::new(EQUAL_EQUAL, "==", L::Null, 1),
-            Token::new(EOF, "", L::Null, 1),
-        ];
-
-        let mut scanner = Scanner::new(source);
-        let tokens = scanner.scan_tokens().expect("Program can be scanned");
-
-        assert_eq!(tokens, expected);
+            assert_eq!(tokens, expected);
+        }
     }
 }
