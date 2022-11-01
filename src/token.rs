af88651fbbd6f53fdc004c002502105a0bda1ab1
Jens Östlund <jostlund@gmail.com>
2022-11-01 11:26:33 +0100

    Handle identifiers and keywords

--- src/token.rs
+++ src/token.rs
@@ -51,10 +51,37 @@ pub enum TokenType {
     EOF,
 }
 
+impl TokenType {
+    pub fn keyword(x: &str) -> Option<TokenType> {
+        match x {
+            "and" => Some(Self::AND),
+            "class" => Some(Self::CLASS),
+            "else" => Some(Self::ELSE),
+            "false" => Some(Self::FALSE),
+            "fun" => Some(Self::FUN),
+            "for" => Some(Self::FOR),
+            "if" => Some(Self::IF),
+            "nil" => Some(Self::NIL),
+            "or" => Some(Self::OR),
+            "print" => Some(Self::PRINT),
+            "return" => Some(Self::RETURN),
+            "super" => Some(Self::SUPER),
+            "this" => Some(Self::THIS),
+            "true" => Some(Self::TRUE),
+            "var" => Some(Self::VAR),
+            "while" => Some(Self::WHILE),
+            _ => None,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Literal {
     Null,
     Number(f64),
+    String(String),
+    Identifier(String),
+    Keyword,
 }
 
 #[derive(Debug, PartialEq)]
@@ -173,8 +200,7 @@ impl<'a> Scanner<'a> {
             ' ' | '\t' | '\r' => Ok(None),
             // New line
             '\n' => {
-                self.line += 1;
-                self.column_offset = self.current;
+                self.newline();
                 Ok(None)
             }
             // Comment or SLASH
@@ -188,6 +214,34 @@ impl<'a> Scanner<'a> {
                 } else {
                     Ok(Some(self.create_token(SLASH)))
                 }
+            }
+            '"' => {
+                let column = self.start - self.column_offset;
+                let start_line = self.line;
+
+                loop {
+                    if let Some(c) = self.advance() {
+                        if c == '"' {
+                            break;
+                        } else if c == '\n' {
+                            self.newline();
+                        }
+                    } else {
+                        return Err(eyre!(
+                            "Line {start_line} Col {column}: Unterminated string!"
+                        ));
+                    }
+                }
+                let lexeme = &self.source[self.start..self.current];
+                let string = &self.source[self.start + 1..self.current - 1];
+
+                Ok(Some(Token::new(
+                    STRING,
+                    lexeme.into(),
+                    Literal::String(string.into()),
+                    start_line,
+                    column,
+                )))
             }
 
             c if c.is_ascii_digit() => {
@@ -210,12 +264,42 @@ impl<'a> Scanner<'a> {
                 )))
             }
 
+            c if c.is_ascii_alphabetic() => {
+                while !self.is_at_end()
+                    && self.chars.peek().map_or(false, |c| {
+                        c.is_ascii_alphabetic() || c.is_ascii_digit() || c == &'_'
+                    })
+                {
+                    self.advance();
+                }
+                let lexeme = &self.source[self.start..self.current];
+
+                let (token_type, literal) = if let Some(keyword) = TokenType::keyword(lexeme) {
+                    (keyword, Literal::Keyword)
+                } else {
+                    (IDENTIFIER, Literal::Identifier(lexeme.into()))
+                };
+
+                Ok(Some(Token::new(
+                    token_type,
+                    lexeme.into(),
+                    literal,
+                    self.line,
+                    self.start - self.column_offset,
+                )))
+            }
+
             _ => {
                 let line = self.line;
                 let col = self.start - self.column_offset;
                 return Err(eyre!("Line {line} Col {col}: Unexpected character '{c}'"));
             }
         }
+    }
+
+    fn newline(&mut self) {
+        self.line += 1;
+        self.column_offset = self.current;
     }
 
     fn create_token(&mut self, t: TokenType) -> Token {
@@ -424,5 +508,213 @@ mod test {
             let tokens = scanner.scan_tokens().unwrap();
             assert_eq!(tokens, expected);
         }
+    }
+
+    #[test]
+    fn strings_are_parsed_correctly() {
+        let test_cases = vec![(
+            "\"a string\"",
+            vec![
+                Token::new(
+                    STRING,
+                    "\"a string\"".into(),
+                    L::String("a string".into()),
+                    1,
+                    0,
+                ),
+                Token::new(EOF, "".into(), L::Null, 1, 10),
+            ],
+        )];
+
+        for (source, expected) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            assert_eq!(tokens, expected);
+        }
+    }
+
+    #[test]
+    fn mutliline_strings_are_parsed_correctly() {
+        let test_cases = vec![(
+            "\"a
+multiline
+string\"",
+            vec![
+                Token::new(
+                    STRING,
+                    "\"a
+multiline
+string\""
+                        .into(),
+                    L::String(
+                        "a
+multiline
+string"
+                            .into(),
+                    ),
+                    1,
+                    0,
+                ),
+                Token::new(EOF, "".into(), L::Null, 3, 7),
+            ],
+        )];
+
+        for (source, expected) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            assert_eq!(tokens, expected);
+        }
+    }
+
+    #[test]
+    fn unterminated_strings_are_handled_correctly() {
+        let mut scanner = Scanner::new("\"unterminated string");
+        match scanner.scan_tokens() {
+            Ok(_) => assert!(false, "Should generate an error!"),
+            Err(e) => assert!(
+                e.to_string().contains("Unterminated string"),
+                "Caused by an unterminated string"
+            ),
+        }
+    }
+
+    #[test]
+    fn identifiers_are_parsed_correctly() {
+        let test_cases = vec![
+            (
+                "foo",
+                vec![
+                    Token::new(IDENTIFIER, "foo".into(), L::Identifier("foo".into()), 1, 0),
+                    Token::new(EOF, "".into(), L::Null, 1, 3),
+                ],
+            ),
+            (
+                "another_example",
+                vec![
+                    Token::new(
+                        IDENTIFIER,
+                        "another_example".into(),
+                        L::Identifier("another_example".into()),
+                        1,
+                        0,
+                    ),
+                    Token::new(EOF, "".into(), L::Null, 1, 15),
+                ],
+            ),
+            (
+                "abc123",
+                vec![
+                    Token::new(
+                        IDENTIFIER,
+                        "abc123".into(),
+                        L::Identifier("abc123".into()),
+                        1,
+                        0,
+                    ),
+                    Token::new(EOF, "".into(), L::Null, 1, 6),
+                ],
+            ),
+        ];
+
+        for (source, expected) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            assert_eq!(tokens, expected);
+        }
+    }
+
+    #[test]
+    fn keywords_are_parsed_correctly() {
+        let test_cases = vec![
+            ("and", AND, 0, 3),
+            ("class", CLASS, 0, 5),
+            ("else", ELSE, 0, 4),
+            ("false", FALSE, 0, 5),
+            ("fun", FUN, 0, 3),
+            ("for", FOR, 0, 3),
+            ("if", IF, 0, 2),
+            ("nil", NIL, 0, 3),
+            ("or", OR, 0, 2),
+            ("print", PRINT, 0, 5),
+            ("return", RETURN, 0, 6),
+            ("super", SUPER, 0, 5),
+            ("this", THIS, 0, 4),
+            ("true", TRUE, 0, 4),
+            ("var", VAR, 0, 3),
+            ("while", WHILE, 0, 5),
+        ];
+
+        for (source, keyword, start, end) in test_cases {
+            let expected = vec![
+                Token::new(keyword, source.into(), L::Keyword, 1, start),
+                Token::new(EOF, "".into(), L::Null, 1, end),
+            ];
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            assert_eq!(tokens, expected);
+        }
+    }
+
+    #[test]
+    fn variable_assignment() {
+        let source = "var x = 42 / (20 + 22);";
+
+        let mut scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens().unwrap();
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token::new(VAR, "var".into(), L::Keyword, 1, 0),
+                Token::new(IDENTIFIER, "x".into(), L::Identifier("x".into()), 1, 4),
+                Token::new(EQUAL, "=".into(), L::Null, 1, 6),
+                Token::new(NUMBER, "42".into(), L::Number(42.), 1, 8),
+                Token::new(SLASH, "/".into(), L::Null, 1, 11),
+                Token::new(LEFT_PAREN, "(".into(), L::Null, 1, 13),
+                Token::new(NUMBER, "20".into(), L::Number(20.), 1, 14),
+                Token::new(PLUS, "+".into(), L::Null, 1, 17),
+                Token::new(NUMBER, "22".into(), L::Number(22.), 1, 19),
+                Token::new(RIGHT_PAREN, ")".into(), L::Null, 1, 21),
+                Token::new(SEMICOLON, ";".into(), L::Null, 1, 22),
+                Token::new(EOF, "".into(), L::Null, 1, 23),
+            ]
+        );
+    }
+
+    #[test]
+    fn function_declaration() {
+        let source = "
+fun square(x) {
+  return x * x;
+}
+"
+        .trim();
+
+        let mut scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens().unwrap();
+
+        let expected = vec![
+            Token::new(FUN, "fun".into(), L::Keyword, 1, 0),
+            Token::new(
+                IDENTIFIER,
+                "square".into(),
+                L::Identifier("square".into()),
+                1,
+                4,
+            ),
+            Token::new(LEFT_PAREN, "(".into(), L::Null, 1, 10),
+            Token::new(IDENTIFIER, "x".into(), L::Identifier("x".into()), 1, 11),
+            Token::new(RIGHT_PAREN, ")".into(), L::Null, 1, 12),
+            Token::new(LEFT_BRACE, "{".into(), L::Null, 1, 14),
+            Token::new(RETURN, "return".into(), L::Keyword, 2, 2),
+            Token::new(IDENTIFIER, "x".into(), L::Identifier("x".into()), 2, 9),
+            Token::new(STAR, "*".into(), L::Null, 2, 11),
+            Token::new(IDENTIFIER, "x".into(), L::Identifier("x".into()), 2, 13),
+            Token::new(SEMICOLON, ";".into(), L::Null, 2, 14),
+            Token::new(RIGHT_BRACE, "}".into(), L::Null, 3, 0),
+            Token::new(EOF, "".into(), L::Null, 3, 1),
+        ];
+
+        assert_eq!(tokens, expected);
     }
 }
