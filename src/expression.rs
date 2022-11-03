25edf0d494f50cd3c7d232fa760e57a93fd714f1
Jens Östlund <jostlund@gmail.com>
2022-11-04 00:27:19 +0100

    Handle binary expressions

--- src/expression.rs
+++ src/expression.rs
@@ -1,4 +1,4 @@
-use crate::token::{self, TokenType, Value};
+use crate::token::{self, TokenType::*, Value};
 
 use eyre::{eyre, Result};
 
@@ -22,17 +22,82 @@ impl<'a> Expr<'a> {
                 Value::Identifier(s) => Ok(Value::String(s.clone())),
                 Value::Keyword(kw) => Ok(Value::Keyword(*kw)),
             },
+
             Expr::Grouping(g) => g.evaluate(),
-            Expr::Unary(t, expr) if t.token_type == TokenType::MINUS => match expr.evaluate()? {
+
+            Expr::Unary(t, expr) if t.token_type == MINUS => match expr.evaluate()? {
                 Value::Number(n) => Ok(Value::Number(n * -1.)),
                 _ => Err(eyre!("The minus operator cannot be applied to {expr}")),
             },
-            Expr::Unary(t, expr) if t.token_type == TokenType::BANG => {
+            Expr::Unary(t, expr) if t.token_type == BANG => {
                 let value = !self.is_truthy(&expr.evaluate()?);
                 Ok(Value::Bool(value))
             }
             Expr::Unary(t, _) => Err(eyre!("Invalid unary token type: {t:?}")),
-            Expr::Binary(_, _, _) => todo!(),
+
+            Expr::Binary(left, op, right)
+                if [MINUS, STAR, SLASH, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL]
+                    .contains(&op.token_type) =>
+            {
+                let left = left.evaluate()?;
+                let right = right.evaluate()?;
+                let (x, y) = if let (Value::Number(l), Value::Number(r)) = (left, right) {
+                    (l, r)
+                } else {
+                    return Err(eyre!("{} can only be applied to numbers", op.lexeme));
+                };
+
+                match op.token_type {
+                    STAR => Ok(Value::Number(x * y)),
+                    MINUS => Ok(Value::Number(x - y)),
+                    SLASH => Ok(Value::Number(x / y)),
+                    GREATER => Ok(Value::Bool(x > y)),
+                    GREATER_EQUAL => Ok(Value::Bool(x >= y)),
+                    LESS => Ok(Value::Bool(x < y)),
+                    LESS_EQUAL => Ok(Value::Bool(x <= y)),
+                    _ => unreachable!(),
+                }
+            }
+            Expr::Binary(left, op, right) if [BANG_EQUAL, EQUAL_EQUAL].contains(&op.token_type) => {
+                let left = left.evaluate()?;
+                let right = right.evaluate()?;
+                match op.token_type {
+                    EQUAL_EQUAL => match (left, right) {
+                        (Value::Number(l), Value::Number(r)) => Ok(Value::Bool(l == r)),
+                        (Value::String(l), Value::String(r)) => Ok(Value::Bool(l == r)),
+                        (Value::Bool(l), Value::Bool(r)) => Ok(Value::Bool(l == r)),
+                        (Value::Null, Value::Null) => Ok(Value::Bool(true)),
+                        _ => Ok(Value::Bool(false)),
+                    },
+                    BANG_EQUAL => match (left, right) {
+                        (Value::Number(l), Value::Number(r)) => Ok(Value::Bool(l != r)),
+                        (Value::String(l), Value::String(r)) => Ok(Value::Bool(l != r)),
+                        (Value::Bool(l), Value::Bool(r)) => Ok(Value::Bool(l != r)),
+                        (Value::Null, Value::Null) => Ok(Value::Bool(false)),
+                        _ => Ok(Value::Bool(true)),
+                    },
+                    _ => unreachable!(),
+                }
+            }
+            Expr::Binary(left, op, right) if op.token_type == PLUS => {
+                let left = left.evaluate()?;
+                let right = right.evaluate()?;
+                match (left, right) {
+                    (Value::Number(l), Value::Number(r)) => Ok(Value::Number(l + r)),
+                    (Value::String(l), Value::String(r)) => Ok(Value::String(format!("{l}{r}"))),
+                    (Value::Number(_), _) => {
+                        Err(eyre!("Right-hand side must also evaluate to a number"))
+                    }
+                    (Value::String(_), _) => {
+                        Err(eyre!("Right-hand  side must also evaluate to a string"))
+                    }
+                    _ => Err(eyre!(
+                        "{} can only be applied to numbers or strings",
+                        op.lexeme
+                    )),
+                }
+            }
+            Expr::Binary(_, op, _) => Err(eyre!("Invalid binary token type: {op:?}")),
         }
     }
 
@@ -114,7 +179,7 @@ mod test {
 
     #[test]
     fn evaluates_literal_expressions() {
-        let values = vec![
+        let values = [
             V::Null,
             V::Bool(true),
             V::Number(3.14),
@@ -129,7 +194,7 @@ mod test {
 
     #[test]
     fn evaluates_grouping_expressions() {
-        let expressions = vec![Expr::Literal(&V::Number(3.14))];
+        let expressions = [Expr::Literal(&V::Number(3.14))];
 
         for e in expressions {
             let expected = e.evaluate().unwrap();
@@ -162,7 +227,7 @@ mod test {
 
     #[test]
     fn minus_unary_cannot_be_applied_to_non_numbers() {
-        let test_cases = vec!["-false", "-\"foo\"", "-null", "-true", "-foo"];
+        let test_cases = ["-false", "-\"foo\"", "-null", "-true", "-foo"];
 
         for source in test_cases {
             let mut scanner = Scanner::new(source);
@@ -201,7 +266,7 @@ mod test {
 
     #[test]
     fn bang_unary_only_treats_nil_and_false_as_falsy() {
-        let negative_cases = vec!["!\"\"", "!0", "!1", "!\"foo\""];
+        let negative_cases = ["!\"\"", "!0", "!1", "!\"foo\""];
 
         for source in negative_cases {
             let mut scanner = Scanner::new(source);
@@ -223,6 +288,97 @@ mod test {
             let value = expression.evaluate().unwrap();
 
             assert_eq!(value, V::Bool(true));
+        }
+    }
+
+    #[test]
+    fn evaluates_arithmatic_expressions() {
+        let test_cases = [
+            ("1 + 1", 2.),
+            ("1 - 1", 0.),
+            ("2 * 2", 4.),
+            ("2 / 2", 1.),
+            ("2 + 4 * 10", 42.),
+            ("(2 + 4) * 10", 60.),
+        ];
+
+        for (source, expected) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            let parser = Parser::new(&tokens);
+            let expression = parser.parse().unwrap();
+            let value = expression.evaluate().unwrap();
+
+            assert_eq!(value, V::Number(expected), "Failed to evaluate {source}");
+        }
+    }
+
+    #[test]
+    fn plus_concatenates_strings() {
+        let source = "\"foo\" + \"bar\"";
+
+        let mut scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens().unwrap();
+        let parser = Parser::new(&tokens);
+        let expression = parser.parse().unwrap();
+        let value = expression.evaluate().unwrap();
+
+        assert_eq!(format!("{value}"), "foobar", "Failed to evaluate {source}");
+    }
+
+    #[test]
+    fn evaluates_comparison_expressions() {
+        let test_cases = [
+            ("2 > 1", true),
+            ("1 < 2", true),
+            ("2 >= 2", true),
+            ("2 <= 2", true),
+            ("2 > 3", false),
+            ("2 >= 3", false),
+            ("3 <= 2", false),
+            ("3 < 2", false),
+        ];
+
+        for (source, expected) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            let parser = Parser::new(&tokens);
+            let expression = parser.parse().unwrap();
+            let value = expression.evaluate().unwrap();
+
+            assert_eq!(value, V::Bool(expected), "Failed to evaluate {source}");
+        }
+    }
+
+    #[test]
+    fn evaluates_equality_expressions() {
+        let test_cases = [
+            ("1 == 1", true),
+            ("nil == nil", true),
+            ("true == true", true),
+            ("\"foo\" == \"foo\"", true),
+            ("1 == 2", false),
+            ("nil == true", false),
+            ("true == false", false),
+            ("\"foo\" == \"bar\"", false),
+            ("1 != 1", false),
+            ("nil != nil", false),
+            ("true != true", false),
+            ("\"foo\" != \"foo\"", false),
+            ("1 != 2", true),
+            ("nil != true", true),
+            ("true != false", true),
+            ("\"foo\" != \"bar\"", true),
+        ];
+
+        for (source, expected) in test_cases {
+            let mut scanner = Scanner::new(source);
+            let tokens = scanner.scan_tokens().unwrap();
+            let parser = Parser::new(&tokens);
+            let expression = parser.parse().unwrap();
+            let value = expression.evaluate().unwrap();
+
+            assert_eq!(value, V::Bool(expected), "Failed to evaluate {source}");
         }
     }
 }
